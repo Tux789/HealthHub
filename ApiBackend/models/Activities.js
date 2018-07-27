c8b36c559bb4de4e55e205e7ada23f39cfaf34d7
Jessica La Torre <jess.latorre@gmail.com>
2018-07-26 21:40:45 -0500

    adjusted activities model in mongoose

--- ApiBackend/models/Activities.js
+++ ApiBackend/models/Activities.js
@@ -1,26 +1,26 @@
-
 // 2 - code validations for passwords etc
-
-const ActionsSchema = new Schema({
-        _userId: {
-            type: Schema.Types.ObjectId,
-            ref: "Headline"
-        },
-
-        goalType: { type: String, required: true },
-        enum: ['TRACK SMOKING', 'TRACK EXERCISE', 'TRACK SLEEP', 'TRACK WEIGHT','IMPROVE SOCIAL INTERACTIONS'],
-        smokingMethod: { type: String, required: true }, 
-        enum: ['CIGARETTE', 'CIGAR', 'E-CIGARETTE', 'PIPE'],
-        cigarettesSmokedPerDay: { type: Number, required: true },
-        howManyOneGramCatridgesPerWeek: { type: Number, required: true },
-        howManyPipesSmokedPerDay: { type: Number, required: true },
-        currentEstimatedHours: {type: Number, required: true }
+var Schema = mongoose.Schema;   /* not sure if we need this line */
+var ActionSchema = new Schema ({
+    
+    _userId: {
+        type: Schema.Types.ObjectId,
+        ref: "Headline"
+    },
+    goalType: { type: String, required: true },
+    goalEnum: ['TRACK SMOKING', 'TRACK EXERCISE', 'TRACK SLEEP', 'TRACK WEIGHT','IMPROVE SOCIAL INTERACTIONS'],
+    smokingMethod: { type: String, required: true }, 
+    smokingEnum: ['CIGARETTE', 'CIGAR', 'E-CIGARETTE', 'PIPE'],
+    cigarettesSmokedPerDay: { type: Number, required: true },
+    cigarsSmokedPerDay: { type: Number, required: true },
+    howManyOneGramCatridgesPerWeek: { type: Number, required: true },
+    howManyPipesSmokedPerDay: { type: Number, required: true },
+    exerciseEnum: ['HIGH INTENSITY', 'MEDIUM INTENSITY', 'LOW INTENSITY'],
+    currentExerciseMinutes: {type: Number, required: true },
+    currenSleepHours: {type: Number, required: true },
+    currentWeight: {type: Number, required: true },
+    currentSocialOutings: {type: Number, required: true },
+    currentSocialHours: {type: Number, required: true },
 });
-
 ///
-const Book = mongoose.model('Activities', ActionsSchema)
-
+const Activities = mongoose.model('Activities', ActionsSchema)
 module.exports = Activities;
-
-const Book = mongoose.model("Book", bookSchema);
-
