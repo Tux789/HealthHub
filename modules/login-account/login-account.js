2f6d33943096396e7a36e1ff289ea57979ac02bc
Jessica La Torre <jess.latorre@gmail.com>
2018-08-04 11:59:51 -0500

    re-added test button to login page

--- modules/login-account/login-account.js
+++ modules/login-account/login-account.js
@@ -107,6 +107,8 @@ testAddRoute = () => {
           <NavButton onPress={() => this.props.navigation.navigate('Info')}> Home </NavButton>
           <NavButton onPress={() => this.props.navigation.navigate('Feed')}> Feed Page</NavButton>
 
+          <NavButton onPress={this.testAddRoute}>Test A thing</NavButton>
+          <NavButton onPress={() => this.props.navigation.navigate('UserDash')}> TestDash </NavButton>
           <Text style={styles.paragraph}>
             You should consult with a healthcare professional before starting any diet, exercise, supplementation or medication program. Statements on Health Hub about products and health conditions have not been evaluated by the U.S. Food and Drug Administration and are not intended to diagnose, treat, cure, or prevent disease. If you have a health concern of any kind consult with your health care professional. Information presented by Living Fuel is for educational purposes only and is not meant to substitute for the advice of a doctor or other medical professional. Health Hub does not make any medical claims or warranties regarding the use of the products listed on this site. The information presented is general in nature and these remedies may not be for everyone. Like other remedies, supplements and herbs do have side effects. You should consult you doctor before taking any medication.
             </Text>
